ea6363770e3504adc5e58baf1aafbc3a219dfb41
mrvnzcn <imervanozcan@gmail.com>
2024-05-01 17:27:24 +0300

    post tag updated

--- BlogApp/Controllers/PostsController.cs
+++ BlogApp/Controllers/PostsController.cs
@@ -143,7 +143,7 @@ namespace BlogApp.Controllers
 
         [Authorize]
         [HttpPost]
-        public IActionResult Edit(PostCreateViewModel model)
+        public IActionResult Edit(PostCreateViewModel model, int[] tagIds)
         {
             if(ModelState.IsValid)
             {
@@ -160,9 +160,10 @@ namespace BlogApp.Controllers
                     entityToUpdate.IsActive = model.IsActive;
                 }
 
-                _postRepository.EditPost(entityToUpdate);
+                _postRepository.EditPost(entityToUpdate, tagIds);
                 return RedirectToAction("List");
             }
+            ViewBag.Tags = _tagRepository.Tags.ToList();
             return View(model);
 
         }
